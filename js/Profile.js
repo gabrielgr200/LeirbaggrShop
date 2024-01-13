10b6ad25554b634ad9505c7ea3d909fb6b9c8bbc
Leirbag <gomesrdg34@gmail.com>
2024-01-12 21:59:24 -0300

    site

--- js/Profile.js
+++ js/Profile.js
@@ -1,74 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchUserData();
-});
-
-async function fetchUserData() {
+  });
+  
+  async function fetchUserData() {
     try {
-        const response = await fetch('https://apiecommerce-316ae4f1fc8b.herokuapp.com/user', {
-            headers: {
-                Authorization: `Bearer ${getToken()}`
-            }
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            displayUserData(data.user);
-        } else {
-            alert(data.mensagem);
+      const response = await fetch('https://apiecommerce-316ae4f1fc8b.herokuapp.com/user', {
+        headers: {
+          Authorization: `Bearer ${getToken()}`
         }
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        displayUserData(data.user);
+      } else {
+        alert(data.mensagem);
+        window.location.href = '/index.html';
+      }
     } catch (error) {
-        console.error(error);
-        alert('Erro ao buscar dados do usuário.');
+      console.error(error);
+      alert('Erro ao buscar dados do usuário.');
     }
-}
-
-function displayUserData(user) {
+  }
+  
+  function displayUserData(user) {
     document.getElementById('name').value = user.name;
     document.getElementById('email').value = user.email;
-}
-
-function getToken() {
+  }
+  
+  function getToken() {
     const userToken = localStorage.getItem('userToken');
     if (userToken) {
-        return userToken;
+      return userToken;
     } else {
-        alert('Token não encontrado ou expirado. Por favor entre.');
-        window.location.href = '/index.html';
+      alert('Token não encontrado ou expirado. Por favor, entre.');
+      window.location.href = '/index.html';
     }
-}
-
-async function deleteAccount() {
+  }
+  
+  async function deleteAccount() {
     try {
-        const response = await fetch('https://apiecommerce-316ae4f1fc8b.herokuapp.com/user', {
-            method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${getToken()}`
-            }
-        });
-
-        if (response.ok) {
-            const modalMessage = document.getElementById('modalMessage');
-            modalMessage.innerText = 'Conta excluída com sucesso.';
-
-            const modal = document.getElementById('myModal');
-            modal.style.display = 'block';
-
-            setTimeout(() => {
-                window.location.href = '/index.html';
-            }, 3000);
-        } else {
-            const data = await response.json();
-            alert(data.mensagem);
+      const response = await fetch('https://apiecommerce-316ae4f1fc8b.herokuapp.com/user', {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${getToken()}`
         }
+      });
+  
+      if (response.ok) {
+        const modalMessage = document.getElementById('modalMessage');
+        modalMessage.innerText = 'Conta excluída com sucesso.';
+  
+        const modal = document.getElementById('myModal');
+        modal.style.display = 'block';
+  
+        setTimeout(() => {
+          window.location.href = '/index.html';
+        }, 3000);
+      } else {
+        const data = await response.json();
+        alert(data.mensagem);
+      }
     } catch (error) {
-        console.error(error);
-        alert('Erro ao excluir a conta.');
+      console.error(error);
+      alert('Erro ao excluir a conta.');
     }
-}
-
-function closeModal() {
+  }
+  
+  function closeModal() {
     const modal = document.getElementById('myModal');
     modal.style.display = 'none';
-}
-
+  }
+  
